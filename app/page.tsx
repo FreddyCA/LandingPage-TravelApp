f9bff325c2d014ee1112f17b26e15c1d3ab00fb0
Freddy Coaricona <fcoaricona@outlook.com>
2024-06-11 20:59:54 -0400

    Add: UI Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+import GetApp from "@/components/GetApp";
+
 export default function Home() {
-  return <h1 className="font-bold text-teal-500 text-2xl">Hola mundo</h1>;
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
 }
